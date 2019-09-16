12b8a9d5d8c3afa4d161521aa087ec2e83dd91d1
unknown <89234857879@mail.ru>
2019-09-16 11:30:18 +0700

    test-modal; version: 0.1

--- script/main.js
+++ script/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
 
+let $modal = $('.modal > .add-modal, .modal > .add-modal-bg');
+
+$('.add-button').click(function() {
+    $modal.show();
+}); // end click
+
+$('.modal-close').click(function() {
+    $modal.hide();
+}); // end click
+
+$('.add-modal-bg').click(function() {
+    $modal.hide();
+}); // end click
+
+$('.modal-name, .modal-description').on('input', function(){
+    var $this = $(this);
+    if ($this.val() == '') {
+        $this.removeClass('modal-name_filled');
+    } else {
+        $this.addClass('modal-name_filled');
+    }
+}); // end on
 
 }); // end ready
